82e4a76c247f6cd28668437c4d76ccd6acbaebbf
Lucas Samuel <lucassamuel.dev@gmail.com>
2023-12-16 12:34:05 -0300

    update styles

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -1,13 +1,7 @@
-import { Lexend_Deca as FontHeading, Inter as FontSans } from 'next/font/google'
+import { Archivo as FontSans } from 'next/font/google'
 
 export const fontSans = FontSans({
 	subsets: ['latin'],
 	display: 'swap',
 	variable: '--font-sans',
-})
-
-export const fontHeading = FontHeading({
-	subsets: ['latin'],
-	display: 'swap',
-	variable: '--font-heading',
 })
