71c5fcb3089b0294cdfb9418bda2a3a6db47ee71
Lucas Samuel <lucassamuel.dev@gmail.com>
2023-12-19 16:54:55 -0300

    create @type and update dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,18 +3,19 @@ import type { Metadata } from 'next'
 import { cn } from '@/lib/utils'
 import { fontSans } from '@/styles/fonts'
 
-import '../styles/globals.css'
+import '@/styles/globals.css'
 
 export const metadata: Metadata = {
   title: 'Login',
   description: 'Desafio 04 - Comunidade Codelândia',
 }
 
-export default function RootLayout({
-  children,
-}: {
+type RootLayoutProps = {
   children: React.ReactNode
-}) {
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
+
   return (
     <html lang="pt-br" className='scroll-smooth' suppressHydrationWarning>
       <body
