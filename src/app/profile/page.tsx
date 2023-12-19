5af3c7038001df3461e8f76bb2f5d935576556f4
Lucas Samuel <lucassamuel.dev@gmail.com>
2023-12-19 18:20:49 -0300

    delete cookie userName

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,21 +1,23 @@
 'use client'
 
 import { Button } from "@/components/ui";
-import { deleteCookie } from "cookies-next";
+import { deleteCookie, getCookie } from "cookies-next";
 import { useRouter } from "next/navigation";
 
 export default function ProfilePage() {
   const router = useRouter()
+  const userName = getCookie('userName')
 
   function LogOutUser() {
     deleteCookie('token')
+    deleteCookie('userName')
     router.push('/')
   }
 
   return (
     <main className="container grid min-h-screen place-items-center">
       <div className="flex flex-col items-center">
-        <h2 className="text-2xl font-bold lg:text-4xl">Olá, seja bem vindo(a)! </h2>
+        <h2 className="text-2xl font-bold lg:text-4xl">Olá {userName} 👋 </h2>
         <p className="text-gray-20 mt-3 text-sm lg:text-lg">O seu login foi realizado com sucesso</p>
 
         <Button
